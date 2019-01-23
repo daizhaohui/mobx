47271b2b3a2ff969494d9d8cc074a96d67318c05
daizhaohui <daizhaohui@wanda.cn>
2019-01-23 11:12:21 +0800

    ok

---
+++ build/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+//const argv = require("yargs").argv;
+const appName = process.env.AppName;
+
+console.log("process.env.appName:" + appName);
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "../src/" + appName + "/index.js"),
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../src/index.html")
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "../dist"),
+    compress: true,
+    port: 8081,
+    open: "Google Chrome",
+    hot: true
+  },
+  devtool: "inline-source-map"
+};
